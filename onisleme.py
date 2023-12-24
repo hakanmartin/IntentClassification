50a3a66c20846f10186bbadc0b1e2e4b3bc7a54b
Hakan Martin <hakanmartin@Hakan-MacBook-Pro.local>
2023-12-24 23:31:31 +0300

    Ikinci commit

---
+++ onisleme.py
@@ -0,0 +1,29 @@
+import spacy
+
+# Türkçe modelini yükle
+nlp = spacy.load("tr_core_news_md")
+
+# Metni işle
+text = "Apple dunyanin en buyuk sirketi."
+doc = nlp(text)
+
+# Tokenization
+formatted_tokens = [f"[{token.text}]" for token in doc]
+
+# Tokenleri tek bir satırda virgülle ayrılmış olarak yazdır
+print(",".join(formatted_tokens))
+
+# Lemmatization
+print("\nLemmatization:")
+for token in doc:
+    print(token.text, token.lemma_)
+
+# Cümle analizi
+print("\nCümleler:")
+for sent in doc.sents:
+    print(sent.text)
+
+# Named Entity Recognition (NER)
+print("\nNamed Entity Recognition (NER):")
+for ent in doc.ents:
+    print(ent.text, ent.label_)
